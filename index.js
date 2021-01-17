9ba437643e517cb6c75e802407259a811b7f47c0
kjablonski08@gmail.com <kjablonski08@gmail.com>
2021-01-17 17:31:50 -0500

    info added to back of card

--- index.js
+++ index.js
@@ -48,17 +48,17 @@ const cards = [
 			'devicon-javascript-plain devicon',
 			'devicon-bootstrap-plain-wordmark devicon',
 		],
-		backSummary: '',
-		demoLink: '',
-		frontendLink: '',
-		backendLink: '',
+		summary:
+			"Loo Loo's Events is a wedding and event planner in Rochester, NY. Planning a wedding can be stressful, but it should be fun. The Loo Loo's Events app is a portfolio page for potential customers to gather information on photos, referrals, and package offerings",
+		demoLink: 'https://looloosevents.netlify.app/',
+		frontendLink: 'https://github.com/KJablonski08/loo-loos-planning-events',
+		backendLink: false,
 		flipped: false,
 	},
 	{
 		title: 'Gitwrap',
 		frontImg:
 			'http://git-wrap.herokuapp.com/static/media/gitwrapLogo.50e6de45.png',
-
 		stack: [
 			'devicon-mongodb-plain-wordmark devicon',
 			'devicon-express-original-wordmark devicon',
@@ -70,10 +70,11 @@ const cards = [
 			'devicon-javascript-plain devicon',
 			'devicon-postgresql-plain-wordmark devicon',
 		],
-		backSummary: '',
-		demoLink: '',
-		frontendLink: '',
-		backendLink: '',
+		summary:
+			'A gift guide application. The app begins with categories (such as "tech", "music", "movies", etc...) and price range options to browse. Choose a gift from the list to read more details and find a purchase link. You can add your gift to a personal favorites list. This will allow you to save your gift ideas for another time',
+		demoLink: 'http://git-wrap.herokuapp.com/',
+		frontendLink: 'https://github.com/anonymous-animals/gitwrap-frontend',
+		backendLink: 'https://github.com/anonymous-animals/gitwrap-backend',
 		flipped: false,
 	},
 	{
@@ -87,10 +88,11 @@ const cards = [
 			'devicon-facebook-plain devicon',
 			'devicon-css3-plain-wordmark devicon',
 		],
-		backSummary: '',
-		demoLink: '',
-		frontendLink: '',
-		backendLink: '',
+		summary:
+			'A collectors app. Users are able to log in, using their email or Facebook, and keep track of their collections. A user has the option to keep their collection anonymous or to post publicly. Each public comic book entry has a comments section for users to comment on each others collections and communicate about the story lines',
+		demoLink: 'https://comic-book-room.herokuapp.com/',
+		frontendLink: 'https://github.com/KJablonski08/comic_book_room',
+		backendLink: false,
 		flipped: false,
 	},
 	{
@@ -101,10 +103,11 @@ const cards = [
 			'devicon-html5-plain-wordmark devicon',
 			'devicon-css3-plain-wordmark devicon',
 		],
-		backSummary: '',
-		demoLink: '',
-		frontendLink: '',
-		backendLink: '',
+		summary:
+			'A search app for choosing your next great read. The app shows the current New York Times best seller lists (fiction and nonfiction). Use the search bar to find a specific book (utilizing the Google Books API). Choose a book from the list to read details and find links to a preview of the chosen book or purchase the e-book',
+		demoLink: 'https://kj-book-browser.herokuapp.com/',
+		frontendLink: 'https://github.com/KJablonski08/book-browser',
+		backendLink: false,
 		flipped: false,
 	},
 	{
@@ -115,10 +118,11 @@ const cards = [
 			'devicon-html5-plain-wordmark devicon',
 			'devicon-css3-plain-wordmark devicon',
 		],
-		backSummary: '',
-		demoLink: '',
-		frontendLink: '',
-		backendLink: '',
+		summary:
+			'A new version of the classic Simon game from Hasbro Gaming, written in vanilla JavaScript Players must repeat random sequences of colors(simulating lights) by pressing the colored boxes in the correct order. As the player selects the correct color, the pattern gets longer by adding one additional color to the sequence each round. Created with accessibility in mind - the game can be played entirely using your keyboard - "Enter" to begin. Arrow keys to navigate the game board: red: ⬆️, yellow: ➡️, blue: ⬇️, green: ⬅️',
+		demoLink: 'kjablonski08.github.io/simon/',
+		frontendLink: 'https://github.com/KJablonski08/simon',
+		backendLink: false,
 		flipped: false,
 	},
 ];
